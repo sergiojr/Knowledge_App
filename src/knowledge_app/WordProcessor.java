96bb14744a596ea83f75f0e7fd8205b942b01684
Sergey Prisakar <prisacar@gmail.com>
2011-11-05 23:01:22 +0400

    * better handling of "missing" 'e'

--- src/knowledge_app/WordProcessor.java
+++ src/knowledge_app/WordProcessor.java
@@ -52,7 +52,7 @@ public class WordProcessor {
 		String tempWord;
 		wordforms = databank.getWordformsByWordformstring(lcWord);
 		if (wordforms == null) {
-			wordforms = databank.getFixedWordForms(lcWord,databank.getPostfix(0));
+			wordforms = databank.getFixedWordForms(lcWord, databank.getPostfix(0));
 			if (!wordforms.isEmpty())
 				if (databank.isOnlyFixedForm(lcWord))
 					return;
@@ -60,33 +60,35 @@ public class WordProcessor {
 			Iterator<Postfix> iterator = postfixes.iterator();
 			while (iterator.hasNext()) {
 				postfix = iterator.next();
-				if (lcWord.endsWith(postfix.getPostfix())){
+				if (lcWord.endsWith(postfix.getPostfix())) {
 					tempWord = lcWord.substring(0, lcWord.length() - postfix.getPostfix().length());
-					wordforms.addAll(databank.getFixedWordForms(tempWord,postfix));
+					wordforms.addAll(databank.getFixedWordForms(tempWord, postfix));
 					if (databank.isOnlyFixedForm(tempWord))
-						return;				
-					wordforms.addAll(parseEnding(tempWord,postfix,0,null));
+						return;
+					wordforms.addAll(parseEnding(tempWord, postfix, 0, null));
 				}
 			}
-			wordforms.addAll(parseEnding(lcWord, databank.getPostfix(0),0,null));
+			wordforms.addAll(parseEnding(lcWord, databank.getPostfix(0), 0, null));
 			databank.putWorformsByWordformstring(lcWord, wordforms);
 		}
 		if (wordforms.isEmpty()) {
-			emptyword = databank.getWord(lcWord, 0, 0, false, 0,0, true);
+			emptyword = databank.getWord(lcWord, 0, 0, false, 0, 0, true);
 			wordforms.add(emptyword.createWordform(lcWord, 0, 0));
 		}
 	}
 
-	private Set<WordForm> parseEnding(String lcWord, Postfix postfix, int complexWordIndex, ComplexWordTemplate complexWordTemplate) throws SQLException {
+	private Set<WordForm> parseEnding(String lcWord, Postfix postfix, int complexWordIndex,
+			ComplexWordTemplate complexWordTemplate) throws SQLException {
 		Set<WordForm> wordforms = new HashSet<WordForm>();
-		//parse simple words
+		// parse simple words
 		for (int i = 0; i <= 5; i++)
 			if (lcWord.length() > i)
 				wordforms.addAll(getEndingWordForms(lcWord.substring(lcWord.length() - i),
-						lcWord.substring(0, lcWord.length() - i), postfix,complexWordIndex,complexWordTemplate));
-		
-		//parse complex words
-		HashSet<ComplexWordTemplate> complexWordTemplates=databank.getComplexWordTemplates();
+						lcWord.substring(0, lcWord.length() - i), postfix, complexWordIndex,
+						complexWordTemplate));
+
+		// parse complex words
+		HashSet<ComplexWordTemplate> complexWordTemplates = databank.getComplexWordTemplates();
 		ComplexWordTemplate curComplexWordTemplate;
 		Iterator<ComplexWordTemplate> iterator = complexWordTemplates.iterator();
 		String[] subWords;
@@ -99,25 +101,31 @@ public class WordProcessor {
 		Word word1;
 		Word word2;
 		Word word;
-		while(iterator.hasNext()){
+		while (iterator.hasNext()) {
 			curComplexWordTemplate = iterator.next();
-			if(lcWord.contains(curComplexWordTemplate.getDelimiter())){
+			if (lcWord.contains(curComplexWordTemplate.getDelimiter())) {
 				subWords = lcWord.split(curComplexWordTemplate.getDelimiter(), 2);
-				word1Wordforms = parseEnding (subWords[0],postfix,1,curComplexWordTemplate);
-				if (word1Wordforms!=null)
-					if(!word1Wordforms.isEmpty())
-						word2Wordforms = parseEnding (subWords[1],postfix,2,curComplexWordTemplate);
-				if (word2Wordforms!=null){
-					word2WordformsIterator=word2Wordforms.iterator();
-					while(word2WordformsIterator.hasNext()){
+				word1Wordforms = parseEnding(subWords[0], postfix, 1, curComplexWordTemplate);
+				if (word1Wordforms != null)
+					if (!word1Wordforms.isEmpty())
+						word2Wordforms = parseEnding(subWords[1], postfix, 2,
+								curComplexWordTemplate);
+				if (word2Wordforms != null) {
+					word2WordformsIterator = word2Wordforms.iterator();
+					while (word2WordformsIterator.hasNext()) {
 						word2Wordform = word2WordformsIterator.next();
-						word1WordformsIterator=word1Wordforms.iterator();
-						while(word1WordformsIterator.hasNext()){
+						word1WordformsIterator = word1Wordforms.iterator();
+						while (word1WordformsIterator.hasNext()) {
 							word1Wordform = word1WordformsIterator.next();
 							word1 = databank.getWord(word1Wordform.wordID);
 							word2 = databank.getWord(word2Wordform.wordID);
-							word = databank.getWord(word1.getWord()+curComplexWordTemplate.getDelimiter()+word2.getWord(), word2Wordform.endingRule.type, word2Wordform.endingRule.rule_no, true, word1.getId(),word2.getId(),true);
-							wordforms.add(word.createWordform(lcWord + postfix.getPostfix(), word2Wordform.endingRule.rule_id, postfix.getId()));
+							word = databank.getWord(
+									word1.getWord() + curComplexWordTemplate.getDelimiter()
+											+ word2.getWord(), word2Wordform.endingRule.type,
+									word2Wordform.endingRule.rule_no, true, word1.getId(),
+									word2.getId(), true);
+							wordforms.add(word.createWordform(lcWord + postfix.getPostfix(),
+									word2Wordform.endingRule.rule_id, postfix.getId()));
 						}
 					}
 				}
@@ -126,8 +134,8 @@ public class WordProcessor {
 		return wordforms;
 	}
 
-	private Set<WordForm> getEndingWordForms(String ending, String base, Postfix postfix, int complexWordIndex, ComplexWordTemplate complexWordTemplate)
-			throws SQLException {
+	private Set<WordForm> getEndingWordForms(String ending, String base, Postfix postfix,
+			int complexWordIndex, ComplexWordTemplate complexWordTemplate) throws SQLException {
 		EndingRule endingrule;
 		EndingRule zeroEndingrule;
 		boolean valid;
@@ -137,7 +145,8 @@ public class WordProcessor {
 		Set<String> modbases;
 		Word word;
 		Set<WordForm> wordforms = new HashSet<WordForm>();
-		Set<EndingRule> endingrules = databank.getEndingRules(ending, postfix, complexWordIndex, complexWordTemplate);
+		Set<EndingRule> endingrules = databank.getEndingRules(ending, postfix, complexWordIndex,
+				complexWordTemplate);
 		Iterator<EndingRule> iterator = endingrules.iterator();
 		Iterator<String> basesIterator;
 		while (iterator.hasNext()) {
@@ -170,13 +179,13 @@ public class WordProcessor {
 				modbases = getZeroForms(base, endingrule, zeroEndingrule);
 				bases.addAll(modbases);
 
-				modbase = dropCharacter(base, endingrule.e_before, 'е');
+				modbase = dropCharacter(base, endingrule.e_before, 'е', 'ь', "л");
 				if (modbase != null) {
 					modbases = getZeroForms(modbase, endingrule, zeroEndingrule);
 					bases.addAll(modbases);
 				}
 
-				modbase = dropCharacter(base, endingrule.o_before, 'о');
+				modbase = dropCharacter(base, endingrule.o_before, 'о', 'ь', "");
 				if (modbase != null) {
 					modbases = getZeroForms(modbase, endingrule, zeroEndingrule);
 					bases.addAll(modbases);
@@ -186,8 +195,10 @@ public class WordProcessor {
 				while (basesIterator.hasNext()) {
 					modbase = basesIterator.next();
 					if (checkBase(modbase, endingrule.type)) {
-						word = databank.getWord(modbase, endingrule.type, endingrule.rule_no, false, 0,0, true);
-						wordforms.add(word.createWordform(base + ending + postfix.getPostfix(), endingrule.rule_id, postfix.getId()));
+						word = databank.getWord(modbase, endingrule.type, endingrule.rule_no,
+								false, 0, 0, true);
+						wordforms.add(word.createWordform(base + ending + postfix.getPostfix(),
+								endingrule.rule_id, postfix.getId()));
 					}
 				}
 			}
@@ -196,17 +207,21 @@ public class WordProcessor {
 	}
 
 	private boolean checkBase(String base, int type) {
-		if (type==numeral){
+		if (type == numeral) {
 			return databank.existNumeral(base);
-		}			
+		}
 		return true;
 	}
 
 	/**
 	 * Changes alternating letters at the end of the base
-	 * @param base - current base
-	 * @param endingrule - current ending rule
-	 * @param zeroEndingrule - ending rule of base word form
+	 * 
+	 * @param base
+	 *            - current base
+	 * @param endingrule
+	 *            - current ending rule
+	 * @param zeroEndingrule
+	 *            - ending rule of base word form
 	 * @return list of bases
 	 */
 	private Set<String> getZeroForms(String base, EndingRule endingrule, EndingRule zeroEndingrule) {
@@ -256,17 +271,22 @@ public class WordProcessor {
 		return modbases;
 	}
 
-	private String dropCharacter(String base, String checkList, char character) {
+	private String dropCharacter(String base, String checkList, char character, char change,
+			String change_after) {
 		String[] baseEnd;
 		String checkEnd;
 		String modbase;
-		if ((checkList != null)&(base.length()>2))
+		if ((checkList != null) & (base.length() > 2))
 			if (!checkList.isEmpty()) {
 				baseEnd = checkList.split(";");
 				for (int i = 0; i < baseEnd.length; i++) {
 					checkEnd = character + baseEnd[i];
 					if (base.endsWith(checkEnd)) {
-						modbase = base.substring(0, base.length() - checkEnd.length()) + baseEnd[i];
+						modbase = base.substring(0, base.length() - checkEnd.length());
+						if (!change_after.isEmpty())
+							if (modbase.endsWith(change_after))
+								modbase = modbase + change;
+						modbase = modbase + baseEnd[i];
 						return modbase;
 					}
 				}
