987a326fefe86b2b39ff01328a7970990cb7601a
Sergey Prisakar <prisacar@gmail.com>
2013-01-05 21:24:33 +0400

    * class that is used to operate with words relation stats

---
+++ src/databank/EndingRuleStat.java
@@ -0,0 +1,17 @@
+package databank;
+
+public class EndingRuleStat {
+	public int type;
+	public int wcase;
+	public int gender;
+	public int sing_pl;
+	public int index;
+	
+	public EndingRuleStat(int type, int wcase, int gender, int sing_pl, int index) {
+		this.type = type;
+		this.wcase = wcase;
+		this.gender = gender;
+		this.sing_pl = sing_pl;
+		this.index = index;
+	}
+}
